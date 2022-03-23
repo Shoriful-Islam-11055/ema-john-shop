2256dc00e7654bc151c49d060478617e15556a34
Shoriful Islam <shoriful15-11055@diu.edu.bd>
2022-03-24 00:20:04 +0600

    tax and shipping add

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -3,17 +3,20 @@ import './Cart.css'
 
 const Cart = ({cart}) => {
     let total = 0;
+    let shipping = 0;
     for(const product of cart){
         total = total + product.price;
+        shipping = shipping + product.shipping;
     }
+    const tax = (total * 0.10).toFixed(3);
     return (
         <div className='cart'>
             <h2 className='text-center py-3'>Order Summary</h2>
             <div className='text-end cart-p'>
                 <p>Selected Items: {cart.length}</p> 
                 <p>Total Price: ${total}</p>
-                <p>Total Shipping: </p>
-                <p>Tax: </p>
+                <p>Total Shipping: ${shipping}</p>
+                <p>Tax: ${tax}</p>
                 <h4>Grand Total: </h4>
             </div>
         </div>
