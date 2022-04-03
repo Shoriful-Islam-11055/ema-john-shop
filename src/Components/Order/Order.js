62a86310cf6406bcfb55735debe429d5a5b48447
Shoriful Islam <shoriful15-11055@diu.edu.bd>
2022-04-03 15:40:40 +0600

    create custom state function

--- src/Components/Order/Order.js
+++ src/Components/Order/Order.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import useProducts from '../../hooks/useProducts';
 import './Order.css'
+
 const Order = () => {
+    const [products, setProducts] = useProducts();
     return (
         <div className='order-container container'>
-            <h2 className='mt-3'>Hello, This is me, Order</h2>
+            <h2 className='mt-3'>Total Products : {products.length}</h2>
         </div>
     );
 };
