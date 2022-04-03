c927ee19cb382b5cffcf14b224104847c7f932e7
Shoriful Islam <shoriful15-11055@diu.edu.bd>
2022-04-03 16:02:49 +0600

    create useCart custom function

--- src/Components/Order/Order.js
+++ src/Components/Order/Order.js
@@ -1,12 +1,15 @@
 import React from 'react';
+import useCart from '../../hooks/useCart';
 import useProducts from '../../hooks/useProducts';
 import './Order.css'
 
 const Order = () => {
     const [products, setProducts] = useProducts();
+    const [cart, setCart] = useCart(products);
     return (
         <div className='order-container container'>
             <h2 className='mt-3'>Total Products : {products.length}</h2>
+            <h3>Total order product : {cart.length}</h3>
         </div>
     );
 };
