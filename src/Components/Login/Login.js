a551ba7ad5caf6e9ee1c05d9113a2627f52ef2b0
Shoriful Islam <shoriful15-11055@diu.edu.bd>
2022-04-16 11:55:22 +0600

    implemented login and signup

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,43 +1,117 @@
-import { faGlobe,  faUserCircle} from '@fortawesome/free-solid-svg-icons'
+import { faGlobe, faUserCircle } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import React from "react";
+import React, { useState } from "react";
 import { Button, Form } from "react-bootstrap";
-import { Link } from "react-router-dom";
+import {useSignInWithEmailAndPassword } from "react-firebase-hooks/auth";
+import { Link, useNavigate } from "react-router-dom";
+import auth from "../../firebase.init";
 import logo from "../../images/hr-line.PNG";
 import "../Register/Register.css";
 
 const Login = () => {
-    return (
-        <div className="register-container vh-100 fixed-top">
-      <Form className="form-area shadow-lg">
+  const [validated, setValidated] = useState(false);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+  const [
+    signInWithEmailAndPassword,
+    user,
+    loading,
+    error,
+  ] = useSignInWithEmailAndPassword(auth);
+
+
+  const handleEmailBlur = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const handlePasswordBlur = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const handleSubmitSignIn = (event) => {
+    event.preventDefault();
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
+    }
+    signInWithEmailAndPassword(email, password);
+    setValidated(true);
+  };
+ 
+  if(user){
+    navigate('/');
+    console.log('hello')
+  }
+
+  return (
+    <div className="register-container vh-100 fixed-top">
+      <Form
+        onSubmit={handleSubmitSignIn}
+        noValidate
+        validated={validated}
+        className="form-area shadow-lg"
+      >
         <h3 className="text-center">Sign In</h3>
-        <p className='text-center fs-1 font-monospace'><FontAwesomeIcon  icon={ faUserCircle }></FontAwesomeIcon></p>
-        <Form.Group className="mb-3" controlId="formGroupEmail">
+        <p className="text-center fs-1 font-monospace">
+          <FontAwesomeIcon icon={faUserCircle}></FontAwesomeIcon>
+        </p>
+        <Form.Group className="mb-3" controlId="validationCustom01">
           <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Control
+            onBlur={handleEmailBlur}
+            required
+            type="email"
+            placeholder="Enter email"
+            defaultValue="hello@gmail.com"
+          />
+          <Form.Control.Feedback type="invalid">
+            Please provide a valid email.
+          </Form.Control.Feedback>
         </Form.Group>
-        <Form.Group className="mb-3" controlId="formGroupPassword">
+
+        <Form.Group className="mb-3" controlId="validationCustom02">
           <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
+          <Form.Control
+            onBlur={handlePasswordBlur}
+            required
+            type="password"
+            placeholder="Enter password"
+          />
+          <Form.Control.Feedback type="invalid">
+            Please provide a valid password.
+          </Form.Control.Feedback>
         </Form.Group>
+        <p style={{color : 'red'}}>{error}</p>
         <div className="d-grid gap-2">
-          <Button variant="secondary" size="lg">
+          <Button type="submit" variant="secondary" size="lg">
             Sign In
           </Button>
         </div>
         <p className="text-center">
-          Don't have account? <Link className='text-decoration-none' to="/register">Create a new account</Link>
+          Don't have account?{" "}
+          <Link className="text-decoration-none" to="/register">
+            Create a new account
+          </Link>
         </p>
-        <img className='img-or' src={logo} alt="" />
+        {
+          loading && <p>Loading....</p> 
+        }
+        <img className="img-or" src={logo} alt="" />
+
         <div className="d-grid gap-2">
           <Button variant="outline-dark" size="lg">
-            <span className='p-2'><FontAwesomeIcon icon={ faGlobe }></FontAwesomeIcon></span>
+            <span className="p-2">
+              <FontAwesomeIcon icon={faGlobe}></FontAwesomeIcon>
+            </span>
             Continue with google
           </Button>
         </div>
       </Form>
     </div>
-    );
+  );
 };
 
 export default Login;
--- src/Components/Register/Register.js
+++ src/Components/Register/Register.js
@@ -1,41 +1,142 @@
-import { faGlobe,  faUserCircle} from '@fortawesome/free-solid-svg-icons'
+import { faGlobe, faUserCircle } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import React from "react";
+import React, { useState } from "react";
 import { Button, Form } from "react-bootstrap";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import logo from "../../images/hr-line.PNG";
 import "./Register.css";
+import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import auth from '../../firebase.init';
 
 const Register = () => {
+  const [validated, setValidated] = useState(false);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confrmPass, setConfrmPass] = useState("");
+  const [error, setError] = useState("");
+  const navigate = useNavigate();
+  
+  const [
+    createUserWithEmailAndPassword,
+    user
+  ] = useCreateUserWithEmailAndPassword(auth);
+ 
+  const handleEmailBlur = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const handlePasswordBlur = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const handleConfrmPasswordBlur = (event) => {
+    setConfrmPass(event.target.value);
+  };
+
+  const handleErrorBlur = (event) => {
+    setError(event.target.value);
+  };
+
+  
+  const handleCreateUser = (event) => {
+    const form = event.currentTarget;
+     event.preventDefault();
+    if (form.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
+      
+    }
+    // if(password !== confrmPass){
+    //   setError('Did not match password');
+    //   return;
+    // }
+
+    // if(password.length < 5){
+    //   setError('Password must be 6 characters or more!');
+    //   event.preventDefault();
+    //   return;
+    // }
+    createUserWithEmailAndPassword(email, password);
+    setValidated(true);
+  };
+
+  if(user){
+    navigate('/login');
+  }
+
+
   return (
     <div className="register-container vh-100 fixed-top">
-      <Form className="form-area shadow-lg">
+      <Form
+        noValidate
+        validated={validated}
+        onSubmit={handleCreateUser}
+        className="form-area shadow-lg"
+      >
         <h3 className="text-center">Sign Up</h3>
-        <p className='text-center fs-1 font-monospace'><FontAwesomeIcon  icon={ faUserCircle }></FontAwesomeIcon></p>
-        <Form.Group className="mb-3" controlId="formGroupEmail">
+        <p className="text-center fs-1 font-monospace">
+          <FontAwesomeIcon icon={faUserCircle}></FontAwesomeIcon>
+        </p>
+        <Form.Group className="mb-2" controlId="validationCustom01">
           <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Control
+            onBlur={handleEmailBlur}
+            required
+            type="email"
+            placeholder="Enter email"
+            defaultValue="hello@gmail.com"
+          />
+          <Form.Control.Feedback type="invalid">
+            Please provide a valid email.
+          </Form.Control.Feedback>
         </Form.Group>
-        <Form.Group className="mb-3" controlId="formGroupPassword">
+
+        <Form.Group className="mb-2" controlId="validationCustom02">
           <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
+          <Form.Control
+            onBlur={handlePasswordBlur}
+            required
+            type="password"
+            placeholder="Enter password"
+          />
+          <p>{error}</p>
+          <Form.Control.Feedback type="invalid">
+            Please provide a valid password.
+          </Form.Control.Feedback>
         </Form.Group>
-        <Form.Group className="mb-3" controlId="formGroupPassword">
+
+        <Form.Group className="mb-3" controlId="validationCustom03">
           <Form.Label>Confirm Password</Form.Label>
-          <Form.Control type="password" placeholder="Confirm Password" />
+          <Form.Control
+            onBlur={handleConfrmPasswordBlur}
+            required
+            type="password"
+            placeholder="Confirm password"
+          />
+          <p style={{color:'red'}}>{error}</p>
+          <Form.Control.Feedback type="invalid">
+            Please provide a valid password.
+          </Form.Control.Feedback>
         </Form.Group>
+
         <div className="d-grid gap-2">
-          <Button variant="secondary" size="lg">
+          <Button type="submit" variant="secondary" size="lg">
             Sign Up
           </Button>
         </div>
         <p className="text-center">
-          Already have an account? <Link className='text-decoration-none' to="/login">Login</Link>
+          Already have an account?{" "}
+          <Link className="text-decoration-none" to="/login">
+            Login
+          </Link>
         </p>
-        <img className='img-or' src={logo} alt="" />
+        <img className="img-or" src={logo} alt="" />
+
         <div className="d-grid gap-2">
           <Button variant="outline-dark" size="lg">
-            <span className='p-2'><FontAwesomeIcon icon={ faGlobe }></FontAwesomeIcon></span>
+            <span className="p-2">
+              <FontAwesomeIcon icon={faGlobe}></FontAwesomeIcon>
+            </span>
             Continue with google
           </Button>
         </div>
